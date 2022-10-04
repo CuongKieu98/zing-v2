5f09cae9098238e1018be7c1a7eba3dac05b8634
Cuong98 <cuongkieu2898@gmail.com>
2022-10-04 17:41:46 +0700

    clean code

--- src/api/apiConfig.js
+++
@@ -1,6 +0,0 @@
-const apiConfig = {
-  baseUrl: "https://api-zing-ver1.herokuapp.com/api",
-
-};
-
-export default apiConfig;
--- src/api/apiUtils.js
+++ src/api/apiUtils.js
@@ -1,12 +1,22 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export function makeApiCall(options){
-    return axios(options).then(response =>{
-        var data = response.data;
-        return data;
-    })
-    .catch(error =>{
-        return error
-    })
+export function handleError(error) {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const navigate = useNavigate();
+  console.error("API call failed:" + error);
+  var errorPage = "/error";
+  navigate(errorPage);
+  throw error;
 }
 
+export function makeApiCall(options) {
+  return axios(options)
+    .then((response) => {
+      var data = response.data;
+      return data;
+    })
+    .catch((error) => {
+      return handleError(error);
+    });
+}
